904fafb72cee72e2c19ee19d8f34e081a6a7704c
Madi 747 <cpt.mahdiyar@gmail.com>
2022-11-05 17:17:18 +0330

    tamrin

---
+++ 2-تمرینات جلسه 12 آبان/tamrin3.cs
@@ -0,0 +1,12 @@
+using System;
+class boolean
+{
+    public static void Main (string [] args)
+    {
+        string number = Console.ReadLine();
+        int num =Convert.ToInt32(number);
+        num= num + 10;
+        Console.WriteLine(num);
+
+    }
+}
